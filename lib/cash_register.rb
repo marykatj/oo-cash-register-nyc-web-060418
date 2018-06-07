43f7f2dba5ee5412185357f4e160ad5169700839
Mary-Kate James <marykatj@gmail.com>
2018-06-07 22:51:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,12 @@
 class CashRegister
 
   attr_reader :discount, :price, :title
-
+  @@items
+  
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @@items << self
   end
 
   def total
@@ -20,5 +22,8 @@ class CashRegister
     return "After the discount, the total comes to #{total}."
   end
 
+  def items
+    @@items
+  end
 
 end
